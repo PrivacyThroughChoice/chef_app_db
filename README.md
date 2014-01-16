8ca917708df8021a1652861685e546f26badc121
Fraser Scott <fraser.scott@burberry.com>
2014-01-16 12:07:14 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# thunr_chef_app_db cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Burberry, LTD (<fraser.scott@burberry.com
+>)
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'thunr_chef_app_db'
+maintainer       'Burberry, LTD'
+maintainer_email 'fraser.scott@burberry.com
+'
+license          'MIT'
+description      'Installs/Configures thunr_chef_app_db'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'mysql'
+depends 'database'
