2a5b5f058f38b1d3caec9e6fba92f99758a6fc35
Fraser Scott <fraser.scott@burberry.com>
2014-01-17 21:00:45 +0000

    adding bind address

--- attributes/mysql.rb
+++ attributes/mysql.rb
@@ -1,3 +1,4 @@
 override['mysql']['server_root_password'] = 'pleasechangeme'
 override['mysql']['server_debian_password'] = 'pleasechangeme'
 override['mysql']['server_repl_password'] = 'pleasechangeme'
+override['mysql']['bind_address'] = 'localhost'
