b85680b38681ede213a0c435c9051c7cfa4920ba
Fraser Scott <fraser.scott@burberry.com>
2014-01-30 15:57:08 +0000

    removing defaults

--- attributes/mysql.rb
+++ attributes/mysql.rb
@@ -1,3 +1,6 @@
 override['mysql']['server_root_password'] = 'pleasechangeme'
 override['mysql']['server_debian_password'] = 'pleasechangeme'
 override['mysql']['server_repl_password'] = 'pleasechangeme'
+override['mysql']['remove_anonymous_users'] = true
+override['mysql']['allow_remote_root'] = false
+override['mysql']['remove_test_database'] = true
