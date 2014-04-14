d092d879455ada4c70a3d710252db16764fd3aee
Fraser Scott <admin@privacythroughchoice.org>
2014-04-14 15:42:44 +0100

    using mysql dump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'admin@privacythroughchoice.org'
 license          'MIT'
 description      'Installs/Configures chef_app_db'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.2.0'
 
 depends 'mysql'
-depends 'database'
+depends 'database', '~> 2.1'
